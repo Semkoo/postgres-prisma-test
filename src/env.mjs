f52e42bf52638599432dbb89a5b949c644bb85f1
Semkoo <semirnik2@gmail.com>
2023-10-02 14:19:30 -0400

    Another atempp2

--- src/env.mjs
+++ src/env.mjs
@@ -52,8 +52,6 @@ export const env = createEnv({
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
     GITHUB_CLIENT_ID: z.string(),
     GITHUB_CLIENT_SECRET: z.string(),
-
-    // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
   },
 
   /**
@@ -80,8 +78,8 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID ?? "",
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET ?? "",
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
