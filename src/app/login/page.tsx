c0b388f8c7a4b1d53c8021c2dc9064761d09d648
Semkoo <semirnik2@gmail.com>
2023-10-02 20:31:54 -0400

    Small change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,13 +3,23 @@ import Link from "next/link";
 
 import { UserAuthForm } from "~/core/Auth/UserAuthForm";
 import MaxWidthWrapper from "~/components/MaxWidthWrapper";
+import { getServerSession } from "next-auth";
+import { authOptions } from "~/core/Auth/authOptions";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Login",
   description: "Login to your account",
 };
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (user) {
+    redirect("/dashboard");
+  }
+
   return (
     <MaxWidthWrapper className="relative flex min-h-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
